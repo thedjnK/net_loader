0e1a7fd10bec45dab15cda6e0de47a80ee6298dc
Jamie McCrae <spam@helper3000.net>
2023-11-22 18:15:11 +0000

    Set network up

--- app/src/main.c
+++ app/src/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+#ifdef CONFIG_NET_DHCPV4
+#include <zephyr/kernel.h>
+#include <zephyr/net/net_if.h>
+#include <zephyr/net/net_core.h>
+#endif
+
 int main(void)
 {
+#ifdef CONFIG_NET_DHCPV4
+	struct net_if *default_network_interface = net_if_get_default();
+	net_dhcpv4_start(default_network_interface);
+#endif
+
+	printk("Ready to receive firmware...");
 	return 0;
 }
