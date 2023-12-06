ed5dd376a222d1a5b42f718a94609df5c2cd709f
Jamie McCrae <spam@helper3000.net>
2023-12-06 19:24:21 +0000

    Add headers

--- app/sysbuild.cmake
+++ app/sysbuild.cmake
@@ -1,3 +1,8 @@
+#
+# Copyright (C) 2023 Jamie M.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 
 set_target_properties(mcuboot PROPERTIES BOARD ${BOARD})
 set_target_properties(app PROPERTIES BOARD ${BOARD}@0)
