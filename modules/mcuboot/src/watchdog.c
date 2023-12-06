e48d057ffff1f156f6d7a11d1cd70cba212bcc61
Jamie McCrae <spam@helper3000.net>
2023-12-06 19:16:55 +0000

    Tidy up

--- modules/mcuboot/src/watchdog.c
+++ modules/mcuboot/src/watchdog.c
@@ -7,7 +7,6 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/watchdog.h>
 
-//#define WATCHDOG_WINDOW (CONFIG_MCUBOOT_WATCHDOG_TIMEOUT * 32768 - 1)
 #define WATCHDOG_WINDOW (CONFIG_MCUBOOT_WATCHDOG_TIMEOUT * 1000)
 
 void mcuboot_status_change(mcuboot_status_type_t status)
@@ -15,6 +14,8 @@ void mcuboot_status_change(mcuboot_status_type_t status)
 	if (status == MCUBOOT_STATUS_STARTUP) {
 		/* Setup watchdog and start it */
 		const struct device *const wdt = DEVICE_DT_GET(DT_ALIAS(watchdog0));
+		int wdt_channel_id;
+		int err;
 
 		if (!device_is_ready(wdt)) {
 			printk("%s: device not ready.\n", wdt->name);
@@ -27,7 +28,6 @@ void mcuboot_status_change(mcuboot_status_type_t status)
 			.window.max = WATCHDOG_WINDOW,
 		};
 
-int wdt_channel_id, err;
 		wdt_channel_id = wdt_install_timeout(wdt, &wdt_config);
 
 		err = wdt_setup(wdt, WDT_OPT_PAUSE_HALTED_BY_DBG);
